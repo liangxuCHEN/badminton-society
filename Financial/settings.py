a09864c364ed5bd274e43a274274fcd8f2d217a4
CHENLiangxu <chenliangxu68@gmail.com>
2016-01-25 09:50:09 +0800

    setting update

--- Financial/settings.py
+++ Financial/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
+    'society'
 )
 
 MIDDLEWARE_CLASSES = (
